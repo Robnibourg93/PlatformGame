e17c10ae722bb036f2eb2c42f18961a9420374b2
Robnibourg93 <robnibourg@hotmail.nl>
2015-11-23 20:59:23 +0100

    Collectible Prefab,fixed bug with UI

--- Assets/Scripts/OrbitCollectible.cs
+++ Assets/Scripts/OrbitCollectible.cs
@@ -1,22 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CirclingObject : MonoBehaviour
+public class OrbitCollectible : MonoBehaviour
 {
 
 
-    GameObject cube;
     public Transform center;
     public Vector3 axis = Vector3.up;
     public Vector3 desiredPosition;
-    public float radius = 2.0f;
+    public float radius = 0.6f;
     public float radiusSpeed = 0.5f;
     public float rotationSpeed = 80.0f;
 
     void Start()
     {
-        cube = GameObject.FindWithTag("Cube");
-        center = cube.transform;
+        center = this.transform.parent;
         transform.position = (transform.position - center.position).normalized * radius + center.position;
         radius = 2.0f;
     }
