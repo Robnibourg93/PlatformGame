897f31a688558fd2ae1404b607f2b75e27f10d7b
Robnibourg93 <robnibourg@hotmail.nl>
2016-05-25 18:07:39 +0200

    Created Main Menu
    added a main menu to the game functioning as starting point

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class Button : MonoBehaviour
+    {
+        private bool pushed = false;
+        public float speed = 2f;
+        Vector3 desiredPosition = new Vector3(0f,3.4f);
+
+        void Start() {
+
+        }
+
+        void Update() {
+            if (pushed)
+            {
+                if (transform.localPosition.y <= -1f) {
+                    pushed = false;
+                }
+                transform.localPosition = Vector3.MoveTowards(transform.localPosition, transform.localPosition - desiredPosition, Time.deltaTime * speed);
+            }
+        }
+
+        void OnTriggerEnter(Collider c)
+        {
+
+            if (c.tag == "Player")
+            {
+                pushed = true;
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/UI/Menu/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+    public Button startButton,exitButton;
+
+	// Use this for initialization
+	void Start () {
+        startButton = startButton.GetComponent<Button>();
+        exitButton = exitButton.GetComponent<Button>();
+    }
+
+    // Update is called once per frame
+    public void ExitPress() {
+        Application.Quit();
+    }
+
+    public void StartPress() {
+        SceneManager.LoadScene("Level 1");
+    }
+
+}
